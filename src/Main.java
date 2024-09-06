95007b13f1fa4950b4ba319b16f51d13fec0e416
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-06 11:50:22 +0200

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    
+    public static void main(String []args){
+        System.out.println("Hola Mundo");
+        
+        String nombre = "Javier";
+        
+        int edad = 24;
+        
+        int a, b, c;
+        a=2;
+        b=5;
+        c=9;
+        
+        System.out.println("Hola " + nombre + " tienes "+ edad);
+        System.out.println(a + b+ c);
+        
+        //Nueva forma de definir variables
+        var primerNombre = "Joselito";
+        var segundoNombre = " De Miguel";
+        var miNombre = primerNombre + segundoNombre;
+        System.out.println(miNombre);
+        System.out.println();
+        
+    }
+}
